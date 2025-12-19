52d52c684fbf40d00cfd0cd4458358736922abef
cosing <cocosing@protonmail.com>
2025-12-19 20:31:11 +0800

    feat: User print state

--- src/testpack/model.py
+++ src/testpack/model.py
@@ -4,6 +4,17 @@ from dataclasses import dataclass as _dataclass
 @_dataclass
 class User:
     name: str
+    age: int
 
     def printname(self):
         print(self.name)
+
+    def print_age_stage(self):
+        if self.age <= 0:
+            print("Illegal age")
+        elif self.age < 18:
+            print("Minor")
+        elif self.age < 60:
+            print("Adult")
+        else:
+            print("Elderly")
--- test/test_user.py
+++ test/test_user.py
@@ -5,21 +5,39 @@ import pytest
 
 def test_user_creation():
     """Tests if a User object can be created with the correct name."""
-    user = User(name="testuser")
+    user = User(name="testuser", age=18)
     assert user.name == "testuser"
 
 
-def test_printname(capsys: pytest.CaptureFixture[str]):
+def test_print_name(capsys: pytest.CaptureFixture[str]):
     """Tests if the printname method prints the correct name to stdout."""
-    user = User(name="testuser")
+    user = User(name="testuser", age=18)
     user.printname()
     captured = capsys.readouterr()
     assert captured.out == "testuser\n"
 
 
-def test_randname(random_str: str):
+def test_random_name(random_str: str):
     """Tests if the printname method prints the correct name to stdout."""
-    user = User(name=random_str)
+    user = User(name=random_str, age=18)
     user.printname()
     logging.info(f"{user.name}")
     assert user.name == random_str
+
+
+@pytest.mark.parametrize(
+    "age, expected_print",
+    [
+        (-1, "Illegal age\n"),
+        (0, "Illegal age\n"),
+        (17, "Minor\n"),
+        (18, "Adult\n"),
+        (60, "Elderly\n"),
+        (100, "Elderly\n"),
+    ],
+)
+def test_age_stages(capsys: pytest.CaptureFixture[str], age: int, expected_print: str):
+    user = User(name="Jone", age=age)
+    user.print_age_stage()
+    captured = capsys.readouterr()
+    assert captured.out == expected_print
